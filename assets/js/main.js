5ec0991507326a433d6ec7e2700099dfd2087163
Alejandro Gonzalez <alejandrogonzalr@gmail.com>
2020-04-20 13:50:51 -0500

    Add projects section

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+(function () {
+    axios
+        .get('https://api.github.com/users/alejandrogonzalr/repos')
+        .then(response => {
+            // console.log(response.data);
+            appendProjects(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})();
+
+function appendProjects(projects) {
+    const projectsArea = document.getElementById('projects');
+
+    projects.sort(sortByDate);
+
+    projects.forEach(project => {
+        if (!project.fork && project.name !== 'alejandrogonzalr.github.io') {
+            const content = `
+            <div class="col-sm-12 col-md-6 col-xl-6">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">${normalizeName(project.name)}</h5>
+                        <h6 class="card-subtitle">${project.language}</h6>
+                        <p>${project.description}</p>
+                        <a target="_blank" href="${project.html_url}">View on Github</a>
+                    </div>
+                </div>
+            </div>
+            `;
+    
+            projectsArea.innerHTML += content;
+        }
+    });
+}
+
+function normalizeName(name) {
+    name = name.split('-').join(' ');
+    name = name.replace(/[A-Z]/g, letter => {
+        return ` ${letter}`;
+    });
+    name = name.replace(/\b\w/g, letter => {
+        return letter.toUpperCase();
+    });
+
+    return name;
+}
+
+function sortByDate(a, b) {
+    return new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime();
+}
--- index.markdown
+++
@@ -1,6 +0,0 @@
----
-layout: default
-title: Home
----
-
-<h5 class="text-center" style="height: 10000px;">Main Content</h5>
